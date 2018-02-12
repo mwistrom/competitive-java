a7f567160d7c1c2adf08f0aeb32a6f66d0e7d7a1
Mark Wistrom <mwistrom@gmail.com>
2018-02-12 10:40:35 -0800

    Stacys Number

---
+++ HackerRankAlgo/algorithm/StacysNumber.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class StacysNumber {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int q = in.nextInt();
+		in.nextLine();
+		
+		char[] s;
+		s = in.nextLine().toCharArray();
+		
+		for (int i = 0; i < q; i ++ ) {
+			int x = in.nextInt();
+		}
+		in.close();
+		
+	}
+}
